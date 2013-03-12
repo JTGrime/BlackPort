0723e1cfcf4fb1fa37cab71a5324d66308b13684
JTGrime <jtgrime@gmail.com>
2013-03-12 00:41:21 +0000

    Resolving naming issue and changing of project Blackport.Test.Web2 to Blackport.Test.Web.

--- Blackport.Test.Web/Properties/AssemblyInfo.cs
+++ Blackport.Test.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blackport.Test.Web")]
+[assembly: AssemblyTitle("Blackport.Test.Web2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blackport.Test.Web")]
+[assembly: AssemblyProduct("Blackport.Test.Web2")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("eaee660b-7c76-4614-96e3-0ae91c5955dc")]
+[assembly: Guid("b28136cb-a605-4561-8751-ae87f41e12eb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
